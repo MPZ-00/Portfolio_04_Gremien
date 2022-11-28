358910ae1a01aeb925a0bdbd0479fe4dea132b1f
Martin <martin.praetzlich@outlook.de>
2022-11-28 13:48:49 +0100

    + 3. Abfrage

--- Portfolio_04_Gremien.sql
+++ Portfolio_04_Gremien.sql
@@ -20,6 +20,20 @@ inner join Gremien g on g.ID = hat.ID_Gremien
 inner join Sitzungen s on s.ID = hat.ID_Sitzungen;
 
 /* 3. Abfrage */
+select distinct Vorname, Nachname, Geschlecht, Geburtsdatum, Strasse, Hausnummer, PLZ, Ort, S.Beginn, S.Ende, S.oeffentlich, S.Einladung_am, S.Ort, T.Titel, T.Kurzbeschreibung, T.Protokolltext
+from nimmt_teil
+inner join Personen Pers on Pers.ID = nimmt_teil.ID_Personen
+left join Professoren on Professoren.ID = Pers.ID
+left join Student on Student.ID = Pers.ID
+left join Mitarbeiter on Mitarbeiter.ID = Pers.ID
+left join Sonstige_Personen on Sonstige_Personen.ID = Pers.ID
+left join Lehrbeauftragte on Lehrbeauftragte.ID = Pers.ID
+inner join Namen on Namen.ID = Pers.ID
+inner join Adresse on Adresse.ID = Pers.ID
+inner join Sitzungen S on S.ID = nimmt_teil.ID_Sitzungen
+inner join top on top.ID_Sitzung = S.ID
+inner join Tagesordnung T on T.ID = top.ID_Tagesordnung
+;
 
 /* 4. Abfrage */
 
