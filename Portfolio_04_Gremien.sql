32387371449bf9f77d4a305c0f39f202ed9ca042
Martin <martin.praetzlich@outlook.de>
2022-11-28 13:15:47 +0100

    + 2. Abfrage

--- Portfolio_04_Gremien.sql
+++ Portfolio_04_Gremien.sql
@@ -4,6 +4,20 @@ inner join Namen on Namen.ID = Personen.ID
 inner join Adresse on Adresse.ID = Personen.ID;
 
 /* 2. Abfrage */
+select
+    g.Name,
+    g.Offiziell,
+    g.Inoffiziell,
+    g.Beginn,
+    g.Ende,
+    s.Beginn as S_Beginn,
+    s.Ende as S_Ende,
+    s.Einladung_am,
+    s.oeffentlich,
+    s.Ort
+from hat
+inner join Gremien g on g.ID = hat.ID_Gremien
+inner join Sitzungen s on s.ID = hat.ID_Sitzungen;
 
 /* 3. Abfrage */
 
